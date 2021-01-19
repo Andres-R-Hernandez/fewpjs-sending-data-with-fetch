efa1d7a35936575d13b9341ef394a0b02a7baaa6
Andres-R-Hernandez <andres.hernandez@utexas.edu>
2021-01-19 17:44:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+const usersURL = "http://localhost:3000/users"
+
+function submitData(name, email) {
+  const data = {
+    name: name,
+    email: email
+  }
+
+  const reqPack = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(data)
+  }
+
+  return fetch(usersURL, reqPack)
+    .then(resp => resp.json())
+    .then(data => renderID(data))
+    .catch(error => renderError(error))
+}
+
+function renderID(data) {
+  document.querySelector("body").innerHTML = data["id"]
+}
+
+function renderError(error) {
+  document.querySelector("body").innerHTML = error.message
+}
+
+submitData("john", "emailz")
